e41ac7aa23c2b0d04ebe86974f60e0576500bf57
dogota_n <dogota_n@etna-alternance.net>
2021-08-25 22:55:01 +0200

    add drag and drop system using react-beautiful-dnd

--- src/components/CityItem.js
+++ src/components/CityItem.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 function CityItem (props) {
     return (
-        <div className="city-item">
+        <>
             <div className="wrapper">
-                <div className="wrapper-item"><span>{ props.data.city_name }</span></div>
+                <div className="wrapper-item wrapper-main"><span>{ props.city_name }</span></div>
                 <div className="wrapper-item-empty"> </div>
                 <div className="wrapper-item-empty"> </div>
             </div>
-            { props.data.brands.map((brand, key) =>
+            { props.brands.map((brand, key) =>
                 <div key={key} className="wrapper">
                     <div className="wrapper-item-empty"> </div>
                     <div className="wrapper-item"><span>{ brand.brand_name }</span></div>
                     <div className="wrapper-item"><span>{ brand.employee_count }</span></div>
                 </div>
             ) }
-        </div>
+        </>
     )
 }
 
--- src/components/TreeView.js
+++ src/components/TreeView.js
@@ -1,25 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import CityItem from "./CityItem";
+import React, { useState, useEffect } from 'react'
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd"
+import CityItem from "./CityItem"
 
 function TreeView() {
-    const [data, setData] = useState({ data: null })
+    const [cityList, updateCityList] = useState([])
 
     useEffect(() => {
         fetch("/.netlify/functions/k1261e599-api")
             .then(response => response.json())
-            .then(json => setData({ data: json.data }))
+            .then(json => updateCityList(json.data))
     }, [])
 
-    if (data.data) {
+    function handleOnDragEnd(result) {
+        if(result.destination)
+            if(result.source.index > result.destination.index) {
+                const items = Array.from(cityList)
+                const [reorderedItem] = items.splice(result.source.index, 1)
+                items.splice(result.destination.index, 0, reorderedItem)
+                updateCityList(items)
+            }
+    }
+
+    if (cityList) {
         return (
-            <div>
-                { data.data.map((data, key) =>
-                    <CityItem
-                        key={key}
-                        data={data}
-                    />
-                ) }
-            </div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+                <Droppable droppableId="city-list">
+                    {(provided) => (
+                        <div className="city-list" {...provided.droppableProps} ref={provided.innerRef}>
+                            { cityList.map(({id, city_name, brands}, index) =>
+                                <Draggable key={id} draggableId={city_name} index={index}>
+                                    {(provided) => (
+                                        <div className="city-item" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                                            <CityItem
+                                                city_name={city_name}
+                                                brands={brands}
+                                            />
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ) }
+                            { provided.placeholder }
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
         )
     } else
         return <div>Loading...</div>
